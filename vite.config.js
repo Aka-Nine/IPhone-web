b18e6780098cbed6bba188f3e981c67b3a42995e
Harsh Yadav <orion260204@gmail.com>
2025-05-30 15:56:22 +0530

    fix vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/IPhone-web/",
+  base: "./",
   plugins: [react()],
   build: {
     rollupOptions: {
